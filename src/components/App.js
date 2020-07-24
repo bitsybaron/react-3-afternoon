5aadac61706049233ae378d098c1537ca3cadeed
bitsybaron <59240206+bitsybaron@users.noreply.github.com>
2020-07-24 16:44:28 -0600

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import Post from './Post/Post'
 
 import './App.css';
 
@@ -19,19 +21,34 @@ class App extends Component {
   }
   
   componentDidMount() {
-
+    axios.get(`https://practiceapi.devmountain.com/api/posts`)
+    .then((results) => {
+      this.setState({
+        posts: results.data
+      })
+    })
   }
 
-  updatePost() {
-  
+  updatePost(id, text) {
+    axios.put(`https://practiceapi.devmountain.com/api/posts?id=${ id }`, { text }).then( results => {
+      this.setState({posts: results.data})
+    })
   }
 
-  deletePost() {
-
+  deletePost(id) {
+    axios.delete(`https://practiceapi.devmountain.com/api/posts?id=${ id }`).then(results => {
+      this.setState({
+        posts: results.data
+      })
+    })
   }
 
-  createPost() {
-
+  createPost(text) {
+    axios.post('https://practiceapi.devmountain.com/api/posts', { text }).then(results => {
+      this.setState({
+        posts: results.data
+      })
+    })
   }
 
   render() {
@@ -43,7 +60,13 @@ class App extends Component {
 
         <section className="App__content">
 
-          <Compose />
+          <Compose createPostFn={this.createPost}/>
+
+          {
+            posts.map( post => (
+              <Post key={post.id} text={post.text} date={post.date} updatePostFn={this.updatePost} id={post.id} deletePostFn={this.deletePost}/>          ))
+          }
+
           
         </section>
       </div>
--- src/components/Compose/Compose.js
+++ src/components/Compose/Compose.js
@@ -16,12 +16,19 @@ export default class Compose extends Component {
     this.createPost = this.createPost.bind( this );
   }
 
+
   updateText( text ) {
     this.setState({ text });
   }
 
   createPost() {
+    const{text} = this.state;
+    const {createPostFn} = this.props;
 
+    createPostFn(text);
+    this.setState ({
+      text: ''
+    })
   }
 
   render() {
